22e142520db30cbc2ede909a4da864335daeaec7
Harihar Subramanyam <hsubrama@mit.edu>
2014-01-26 09:57:05 -0500

    Started class

---
+++ video_survey.js
@@ -0,0 +1,30 @@
+/*
+ * videoElement = the video element on the page which will display the videos
+ * questionDiv = div to show the question/answers in
+ * videoURLs = the list of urls of the videos to show
+ * QAs = an array of question data. Takes the form
+ *	[QA1, QA2, ...]
+ *	where each QA takes the form
+ *	{
+ *		questionTitle:"What should I put here?"
+ *		responses:["FirstResponse", "SecondResponse", "ThirdResponse", ...]
+ *		videoURL:"the/video/url/this/question/is/associated/with"
+ *	} 
+ */
+function VideoQuestion(videoElement, questionDiv, videoURLs, QAs){
+	this.videoElement = videoElement;
+	this.QAs = QAs;
+	this.videoURLs = videoURLs;
+	this.loadVideo = function(url){
+		var sources = this.videoElement.getElementsByTagName("source");
+		sources[0].src = url;
+	};
+	this.nextVideo = function(){
+		var firstElement = this.videoURLs.shift();
+		this.videoURLs.push(firstElement);
+		return firstElement;
+	};
+	this.loadNextVideo = function(){
+		this.loadVideo(this.nextVideo());
+	};
+}
