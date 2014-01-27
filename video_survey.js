f7ae777b8b79cc5b49538ddef2cbc2f4017fc4ee
Harihar Subramanyam <hsubrama@mit.edu>
2014-01-26 19:42:04 -0500

    Modal on display video

--- video_survey.js
+++ video_survey.js
@@ -26,20 +26,36 @@ function SurveyManager(videoElement, questionDiv, surveyVideos){
 		}
 	};
 
+	// Displays the next video
 	this.displayNextVideo = function(){
+		this.videoElement.pause();
 		var currentSurveyVideo = this.surveyVideos[currentSurveyVideoIndex];
-		confirm(currentSurveyVideo.preVideoPrompt);
-		this.loadVideo(currentSurveyVideo.videoURL);
-		this.videoElement.play();
-		currentSurveyVideoIndex = (currentSurveyVideoIndex + 1)%this.surveyVideos.length;
+		$("#my_modal_title").text(currentSurveyVideo.videoTitle);
+		$("#my_modal_body").text(currentSurveyVideo.preVideoPrompt);
+		$("#my_modal").modal();
+		var thisObject = this;
+		$("#my_modal_button").unbind("click");
+		$("#my_modal_button").click(function(){
+			thisObject.loadVideo(currentSurveyVideo.videoURL);
+			thisObject.videoElement.play();
+			console.log(currentSurveyVideoIndex);
+			currentSurveyVideoIndex = (currentSurveyVideoIndex + 1)%thisObject.surveyVideos.length;
+		});
 	};
 
+	// Load the video for the given URL
 	this.loadVideo = function(url){
 		var sources = this.videoElement.getElementsByTagName("source");
 		sources[0].src = url;
 		this.videoElement.load();
 		return url;
 	};
+
+	this.getBootstrapModalHTML = function(){
+		return '<div class="modal fade" id="my_modal"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title" id="my_modal_title">Modal title</h4></div><div class="modal-body"><p id="my_modal_body">One fine body&hellip;</p></div><div class="modal-footer"><button type="button" class="btn btn-primary" data-dismiss="modal" id="my_modal_button">OK</button></div></div><!-- /.modal-content --></div><!-- /.modal-dialog --></div><!-- /.modal -->';
+	};
+
+	$('body').append(this.getBootstrapModalHTML());
 	this.shuffleVideoSurveys();
 }
 
@@ -58,7 +74,8 @@ function QA(question, responses){
  * preVideoPrompt = the prompt text that will be displayed before the video begins (ex. "You will see a video of a bunch of colors and then asked a set of questions afterwards")
  * timedQA = the question to show while the video is playing (and should keep track of the time taken to answer the question) - if there isn't any timed question, then pass in null
  */
-function SurveyVideo(videoURL, QAs, preVideoPrompt, timedQA){
+function SurveyVideo(videoURL, QAs, videoTitle, preVideoPrompt, timedQA){
+	this.videoTitle = videoTitle;
 	this.videoURL = videoURL;
 	this.preVideoPrompt = preVideoPrompt;
 	this.QAs = QAs;
